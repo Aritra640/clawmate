5a174ef5140d977d1a46202e20d9fb898051841b
Aritra640 <chatterjeearitra15@gmail.com>
2026-07-01 23:55:03 +0530

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{execute, cursor, terminal};
+use std::io::stdout;
+
+fn main() -> std::io::Result<()> {
+    let mut out = stdout();
+    execute!(out, terminal::Clear(terminal::ClearType::All))?;
+
+    for row in 0..8 {
+        for col in 0..8 {
+            execute!(out, cursor::MoveTo(col * 2, row))?;
+            print!(".");
+        }
+    }
+
+    Ok(())
 }
