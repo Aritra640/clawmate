76bdecd4ac8052240421fd7323a69bb119321ae7
Aritra640 <chatterjeearitra15@gmail.com>
2026-07-20 21:52:45 +0530

    minor changes

--- clawengine/src/lib.rs
+++ clawengine/src/lib.rs
@@ -2,3 +2,4 @@ pub mod square;
 pub mod piece;
 pub mod board;
 pub mod chessmove;
+pub mod movegen;
---
+++ clawengine/src/movegen.rs
@@ -0,0 +1,11 @@
+use crate::{board::Board, chessmove::ChessMove};
+
+pub struct MoveGenerator;
+
+impl MoveGenerator {
+    pub fn generate(board: &Board) -> Vec<ChessMove> {
+        let mut moves = Vec::new();
+
+        moves
+    }
+}
